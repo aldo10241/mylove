8199a1c7947c9e1bf4f46f03a501d5a7042efa16
Aldo <angeluriel99@hotmail.com>
2024-07-31 03:41:26 -0600

    new design

---
+++ src/main.js
@@ -0,0 +1,8 @@
+var texto = document.getElementById('text')
+var carta = "<p>Feliz aniversario corazón<br> Que otra cosa puedo decir además de que soy muy feliz a tu lado, sabemos que no todo puede ser perfecto u Te amo!</p";
+function mensaje() {
+    if (texto === texto) {
+        texto.innerHTML = carta;
+
+    }
+}
