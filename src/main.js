03717fee97e719eb4503a03513a0e674bd5e0af9
Aldo <angeluriel99@hotmail.com>
2024-07-31 23:51:34 -0600

    fix changes

--- src/main.js
+++ src/main.js
@@ -1,25 +1,23 @@
 var elemento = document.getElementById('carta')
-var carta = "<h1 class='font-extrabold text-2xl italic text-purple-500'>Feliz aniversario corazón</h1><br><p class='text-center italic text-gray-500'>Todo este tiempo que hemos pasado juntos ha sido sumamente bonito,<br>lleno de experiencias, palabras, recuerdos, sentimientos que me llenan el corazón de alegría,<br>quiero pedirte que vivamos el presente amándonos y dando todo lo bueno que tenemos,<br>te amo, me importas mucho y quiero seguir conquistando ese corazón tan grande y lindo que tienes,<br>te mereces muchas cosas y quiero que vivamos el ahora con mucho cariño apoyándonos en todo lo que nos hace felices,<br>saliendo adelante juntos, compartiendo nuestros logros y creando nuevos momentos juntos,<br>quiero decirte todos los días lo mucho que te quiero, dejame segui siendo parte de tu vida, me encantas.🫂</p";
+var carta = "<h1 class='font-extrabold text-2xl italic text-purple-500'>Feliz aniversario corazón</h1><br><p class='text-center italic text-gray-500'>Todo este tiempo que hemos pasado juntos ha sido sumamente bonito, lleno de experiencias, palabras, recuerdos, sentimientos que me llenan el corazón de alegría, quiero pedirte que vivamos el presente amándonos y dando todo lo bueno que tenemos, te amo, me importas mucho y quiero seguir conquistando ese corazón tan grande y lindo que tienes, te mereces muchas cosas y quiero que vivamos el ahora con mucho cariño apoyándonos en todo lo que nos hace felices, saliendo adelante juntos, compartiendo nuestros logros y creando nuevos momentos juntos, quiero decirte todos los días lo mucho que te quiero, dejame segui siendo parte de tu vida, me encantas.🫂</p";
 
-var elemento2 = document.getElementById('imagen');
-var imagen = "<img class='w-full max-w-56 rounded-full mb-8' src='https://goo.su/islmwe9' alt=''>"
+// var elemento2 = document.getElementById('imagen');
+// var imagen = "<img class='w-full max-w-56 rounded-full mb-8' src='https://goo.su/islmwe9' alt=''>"
 
 function mensaje() {
     elemento.classList.remove('fade-in');
     elemento.classList.add('fade-out');
 
-    elemento2.classList.remove('fade-in');
-    elemento2.classList.add('fade-out');
-
-
+    // elemento2.classList.remove('fade-in');
+    // elemento2.classList.add('fade-out');
 
     setTimeout(function () {
         elemento.innerHTML = carta;
         elemento.classList.remove('fade-out');
         elemento.classList.add('fade-in');
-        elemento2.innerHTML = imagen;
-        elemento2.classList.remove('fade-out');
-        elemento2.classList.add('fade-in');
+        // elemento2.innerHTML = imagen;
+        // elemento2.classList.remove('fade-out');
+        // elemento2.classList.add('fade-in');
     }, 1000); // Tiempo de la transición (1s)
 }
 
